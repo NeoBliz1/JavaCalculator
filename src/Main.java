cd9396c19c79db30e8ac42adc8a6d270e33d48f8
ususer <neoBliz1>
2023-01-20 22:19:28 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Main {
+    static String calc(String equation) {
+
+        return equation;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calc(args[0]));
+    }
+}
